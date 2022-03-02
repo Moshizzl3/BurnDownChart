282b87fae883c6d59edb0ff10321c33cdccba44b
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-03-02 17:23:07 +0100

    first commit

---
+++ src/main/java/com/example/burndownchartproject/BurnDownChartProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.burndownchartproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurnDownChartProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurnDownChartProjectApplication.class, args);
+    }
+
+}
