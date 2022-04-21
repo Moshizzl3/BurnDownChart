abc8dc452fdc8b57237286cb86edc38035e4f87a
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-04-21 11:27:38 +0200

    heeej

--- src/test/java/com/example/burndownchartproject/BurnDownChartProjectApplicationTests.java
+++ src/test/java/com/example/burndownchartproject/BurnDownChartProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.burndownchartproject;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,5 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @DataJpaTest
 class BurnDownChartProjectApplicationTests {
 
-
+    @Test
+    public void testFun(){
+        Assertions.assertEquals(3, 1+2);
+    }
 }
